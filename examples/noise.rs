2fa8992343e3344031df5fea7e1d60f3edc909f9
Ty Overby <ty@pre-alpha.com>
2015-01-03 22:49:00 -0800

    added noise example

---
+++ examples/noise.rs
@@ -0,0 +1,30 @@
+extern crate lux;
+extern crate noise;
+extern crate nd_iter;
+
+use lux::{Window, LuxCanvas, Interactive, colors};
+use nd_iter::iter_2d;
+
+const DIV: f32 = 30.0;
+
+fn main() {
+    let mut window = Window::new().unwrap();
+
+    let seed = noise::Seed::new(0);
+    let mut z = 0i32;
+
+    while window.is_open() {
+        z += 1;
+        let mut frame = window.frame(colors::RED);
+
+        frame.draw_pixels(
+            iter_2d(range(0i32, 256), range(0i32, 245)).map(|(x, y)| {
+                let (x, y) = (x as f32, y as f32);
+                let value = noise::simplectic3(&seed, &[x / DIV, y / DIV, z as f32 / DIV]);
+                let value = (value + 1.0) / 2.0;
+
+                ((x, y), [value, value, value])
+            })
+        );
+    }
+}
