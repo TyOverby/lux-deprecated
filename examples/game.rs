501fbf120be7ae5f4a8ea036017ff539c01c1568
Ty Overby <ty@pre-alpha.com>
2015-04-24 11:39:56 -0700

    Revert "Set up for support of different used arrow keys."
    This reverts commit 1f8505a7e2017d6df989d692f058c4d201091f3c.

--- examples/game.rs
+++ examples/game.rs
@@ -21,11 +21,6 @@ impl MyGame {
 
 impl Game for MyGame {
     fn update(&mut self, dt: f32, window: &mut Window, _events: &mut EventIterator) {
-        let up_key = 'w';
-        let left_key = 'a';
-        let down_key = 's';
-        let right_key = 'd';
-
         // position
         self.pos.0 += self.speed.0 * dt;
         self.pos.1 += self.speed.1 * dt;
@@ -38,15 +33,15 @@ impl Game for MyGame {
 
         // events
         // x
-        if window.is_key_pressed(left_key) {
+        if window.is_key_pressed('a') {
             self.speed.0 = -MOVEMENT_SPEED;
-        } else if window.is_key_pressed(right_key) {
+        } else if window.is_key_pressed('d') {
             self.speed.0 = MOVEMENT_SPEED;
         }
         // y
-        if window.is_key_pressed(up_key) {
+        if window.is_key_pressed('w') {
             self.speed.1 = -MOVEMENT_SPEED;
-        } else if window.is_key_pressed(down_key) {
+        } else if window.is_key_pressed('s') {
             self.speed.1 = MOVEMENT_SPEED;
         }
         // ::std::thread::sleep_ms(5);
