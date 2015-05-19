f80985f5d881b0343b618c8e3734e206dbef7141
Ty Overby <tyoverby@frostling.cs.washington.edu>
2015-05-19 13:45:16 -0700

    rework game example

--- examples/game.rs
+++ examples/game.rs
@@ -17,13 +17,25 @@ impl Game for MyGame {
     }
 
     fn update(&mut self, dt: f32, window: &mut Window, _events: &mut EventIterator) -> LuxResult<()> {
-        // position
+        fn clamp(low: f32, value: f32, high: f32) -> f32 {
+            if value < low {
+                low
+            } else if value > high {
+                high
+            } else {
+                value
+            }
+        }
+
+        // Update position
         self.pos.0 += self.speed.0 * dt;
         self.pos.1 += self.speed.1 * dt;
+
         // Keep the player from moving off the edge
         self.pos.0 = clamp(0.0, self.pos.0, window.width() - PLAYER_SIZE);
         self.pos.1 = clamp(0.0, self.pos.1, window.height() - PLAYER_SIZE);
-        // dampening
+
+        // Apply speed dampening
         self.speed.0 *= 1.0 - dt;
         self.speed.1 *= 1.0 - dt;
 
@@ -45,13 +57,13 @@ impl Game for MyGame {
     }
 
     fn render(&mut self, lag: f32, _window: &mut Window, frame: &mut Frame) -> LuxResult<()> {
-        //let lag = 0.0;
-        let (x, y) = self.pos;
+        let (px, py) = self.pos;
         let (vx, vy) = self.speed;
 
         try!(frame.text("Use the [w][a][s][d] keys to move around", 5.0, 5.0).draw());
         try!(frame.text("Hold the spacebar to see the debug fps viewer", 5.0, 25.0).draw());
-        frame.circle(x + vx * lag, y + vy * lag, PLAYER_SIZE).fill();
+
+        frame.circle(px + vx * lag, py + vy * lag, PLAYER_SIZE).fill();
 
         Ok(())
     }
@@ -60,16 +72,6 @@ impl Game for MyGame {
 
     fn show_fps(&self, window: &Window) -> bool {
         window.is_key_pressed(' ')
-    }
-}
-
-fn clamp(low: f32, value: f32, high: f32) -> f32 {
-    if value < low {
-        low
-    } else if value > high {
-        high
-    } else {
-        value
     }
 }
 
