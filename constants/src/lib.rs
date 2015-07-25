eb70c8a8748a49121787c985e9150db59303087b
Ty Overby <ty@pre-alpha.com>
2015-07-24 23:26:46 -0700

    constants are static instead of const

--- constants/src/lib.rs
+++ constants/src/lib.rs
@@ -1,18 +1,18 @@
-pub const SOURCE_CODE_PRO_REGULAR: &'static[u8] =
+pub static SOURCE_CODE_PRO_REGULAR: &'static[u8] =
     include_bytes!("../../resources/SourceCodePro-Regular.ttf");
 
 // Rendered images
-pub const SCP_12_PNG: &'static[u8] =
+pub static SCP_12_PNG: &'static[u8] =
     include_bytes!("../../resources/SourceCodePro-Regular-12.png");
-pub const SCP_20_PNG: &'static[u8] =
+pub static SCP_20_PNG: &'static[u8] =
     include_bytes!("../../resources/SourceCodePro-Regular-20.png");
-pub const SCP_30_PNG: &'static[u8] =
+pub static SCP_30_PNG: &'static[u8] =
     include_bytes!("../../resources/SourceCodePro-Regular-30.png");
 
 // Info files
-pub const SCP_12_BINCODE: &'static[u8] =
+pub static SCP_12_BINCODE: &'static[u8] =
     include_bytes!("../../resources/SourceCodePro-Regular-12.bincode");
-pub const SCP_20_BINCODE: &'static[u8] =
+pub static SCP_20_BINCODE: &'static[u8] =
     include_bytes!("../../resources/SourceCodePro-Regular-20.bincode");
-pub const SCP_30_BINCODE: &'static[u8] =
+pub static SCP_30_BINCODE: &'static[u8] =
     include_bytes!("../../resources/SourceCodePro-Regular-30.bincode");
